f2bc603a56a0f1841a99d303617da96fe323b140
mypicturesteam <79681891+mypicturesteam@users.noreply.github.com>
2021-03-11 08:53:19 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x
-/usr/local/bin/cpuminer -a yespower -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u "$CRYPTO_WALLET_MINING" -t "$THREAD_COUNT"
+/usr/local/bin/cpuminer -a yespowerSUGAR -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u "$CRYPTO_WALLET_MINING" -t "$THREAD_COUNT"
